2b34e5331af6559fb9855b8010f19de03334fad4
poojamehta46 <pooja.mehta46@gmail.com>
2026-02-11 22:38:09 +0530

    router

--- src/App.js
+++ src/App.js
@@ -4,11 +4,17 @@ import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import Alert from './components/Alert';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+
 
 function App() {
   const [mode, setMode] = useState('light');
   const [alert, setAlert] = useState(null);
-
+ 
   const showAlert = (message, type) =>{
     setAlert ({
       msg : message,
@@ -24,21 +30,29 @@ function App() {
       setMode('dark')
       document.body.style.backgroundColor = 'grey'
       showAlert("Dark mode has been enabled!", "success")
+      document.title = "TextUtils- Dark Mde"
     }
     else{
       setMode('light')
       document.body.style.backgroundColor = 'white'
       showAlert("Light mode has been enabled!", "success")
+      document.title = "TextUtils- Light Mde"
     }
   }
+
+  
   return (
-    <>    
+    <>  
+    <Router> 
     <Navbar titleText = "Textutils" aboutText= "About" mode = {mode} toggleMode={toggleMode}/> 
     <Alert alert = {alert}/>
     <div className = "container my-3" >
-    <TextForm showAlert ={showAlert} heading = "Enter your text  here" mode = {mode}/>
-    <About/>
+    <Routes>
+      <Route exact path="/about" element={<About />} /> 
+      <Route exact path="/" element = {<TextForm showAlert ={showAlert}  heading = "Enter your text  here" mode = {mode}/>} />
+    </Routes>
     </div>
+    </Router> 
     </>
 );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,31 +1,36 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Link } from "react-router-dom"
 
 export default function Navbar (props) {
+  
+
+  // const onClick = (event)=>{
+  //   console.log("onClick button clicked")
+  //   setText(event.target.value)
+  // }
     return (
         <nav className={`navbar navbar-expand-lg bg-body-tertiary bg-${props.mode}`} data-bs-theme={`${props.mode}`}>
         <div className="container-fluid">
-          <a className="navbar-brand" href="/#">{props.titleText}</a>
+          <Link className="navbar-brand" to="/#">{props.titleText}</Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/#">{props.homeText}</a>
+                <Link className="nav-link active" aria-current="page" to="/#">{props.homeText}</Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="/#">{props.aboutText}</a>
+                <Link className="nav-link" to="/About">{props.aboutText}</Link>
               </li>
-            </ul>
-            {/* <form className="d-flex" role="search">
-              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-              <button className="btn btn-outline-success" type="submit">Search</button>
-            </form> */}
+            </ul> 
             <div className={`form-check form-switch text-${props.mode==='light'?'dark':'light'}`}>
               <input className="form-check-input" onClick={props.toggleMode} type="checkbox" role="switch" id="switchCheckDefault"/>
-              <label className="form-check-label" htmlfor="switchCheckDefault">Enable DarkMode</label>
+              <label className="form-check-label" htmlFor="switchCheckDefault">Enable DarkMode</label>
             </div>
+
+       
           </div>
         </div>
       </nav>
